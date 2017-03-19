d881eae9415c1220aa14eb6068300f590efc4f20
Ian Tan <kishontcf@gmail.com>
2017-03-19 20:50:58 +0800

    ...

--- src/main/java/seedu/address/logic/commands/SearchCommand.java
+++ src/main/java/seedu/address/logic/commands/SearchCommand.java
@@ -9,36 +9,36 @@ import seedu.address.logic.commands.exceptions.EmptySearchException;
 
 //@@author A0139322L
 public class SearchCommand extends Command {
-	
-	public static final String COMMAND_WORD = "search";
-	public static final String MESSAGE_SEARCH_SUCCESS = "The search results have been filtered.";
-	public static final String MESSAGE_EMPTY_SEARCH = "No results were found!";
-	
-	public static int[] searchResults;
-	public final Set<String> keyword;
-	
-	/**
-	 * Creates a SearchCommand using the given keyword.
-	 * 
-	 * @param keyword
-	 * @throws IllegalValueException if the keyword is invalid.
-	 */
-	public SearchCommand(String keyword) throws IllegalValueException {
-		this.keyword = new HashSet<String>();
-		this.keyword.add(keyword);
-	}
-	
-	
-	@Override
-	public CommandResult execute() throws EmptySearchException {
-		
-		if (keyword.size() <= 0) {
-			throw new EmptySearchException(MESSAGE_EMPTY_SEARCH);
-		}
-		
-		// filter the list according to the search
-		model.updateFilteredTaskList(keyword);
-		
-		return new CommandResult(MESSAGE_SEARCH_SUCCESS);
-	}
+
+    public static final String COMMAND_WORD = "search";
+    public static final String MESSAGE_SEARCH_SUCCESS = "The search results have been filtered.";
+    public static final String MESSAGE_EMPTY_SEARCH = "No results were found!";
+
+    public static int[] searchResults;
+    public final Set<String> keyword;
+
+    /**
+    * Creates a SearchCommand using the given keyword.
+    *
+    *
+    **@param keyword
+    @throws IllegalValueException if the keyword is invalid.
+    */
+    public SearchCommand(String keyword) throws IllegalValueException {
+        this.keyword = new HashSet<String>();
+        this.keyword.add(keyword);
+    }
+
+    @Override
+    public CommandResult execute() throws EmptySearchException {
+
+        if (keyword.size() <= 0) {
+            throw new EmptySearchException(MESSAGE_EMPTY_SEARCH);
+        }
+
+        // filter the list according to the search
+        model.updateFilteredTaskList(keyword);
+
+        return new CommandResult(MESSAGE_SEARCH_SUCCESS);
+    }
 }
