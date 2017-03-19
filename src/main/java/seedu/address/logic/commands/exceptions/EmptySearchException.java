5e33b585c7827c0062bd87a4af0784463e705769
Ian Tan <kishontcf@gmail.com>
2017-03-19 13:26:29 +0800

    Basic structure of SearchCommand (along with included exceptions).

---
+++ src/main/java/seedu/address/logic/commands/exceptions/EmptySearchException.java
@@ -0,0 +1,8 @@
+package seedu.address.logic.commands.exceptions;
+
+//@@author A0139322L
+public class EmptySearchException extends CommandException {
+	public EmptySearchException(String message) {
+		super(message);
+	}
+}
