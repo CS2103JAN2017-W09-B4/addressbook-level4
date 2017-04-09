ec9f0ce128af8cdcb73c41e2a726b9eb2b578d18
Qixiang <xu.qixiang.47@gmail.com>
2017-04-10 04:57:17 +0800

    Updated JUnit Test for FindCommandTest

--- src/test/java/guitests/FindCommandTest.java
+++ src/test/java/guitests/FindCommandTest.java
@@ -7,40 +7,74 @@ import org.junit.Test;
 import seedu.address.testutil.TestTask;
 import seedu.task.commons.core.Messages;
 
+//A0135762A
 public class FindCommandTest extends TaskManagerGuiTest {
 
     @Test
-    public void find_nonEmptyList() {
-        assertFindResult("find Mark"); // no results
-        assertFindResult("find Task44", td.task44); // multiple results
+    public void find_exactMatchCases() {
+        assertFindExactMatchResult("find Random"); // no results
+        assertFindExactMatchResult("find Task11", td.task11); // multiple results
+        assertFindExactMatchResult("find tASK11", td.task11); // multiple results, not case sensitive
 
-        //find after deleting one result
+        // find after deleting one task
+        commandBox.runCommand("list");
         commandBox.runCommand("delete 1");
-        assertFindResult("find Task77", td.task77);
+        assertFindExactMatchResult("find Task11");
+        assertFindExactMatchResult("find Task33", td.task33);
 
-        //find task after deleting it
-        commandBox.runCommand("delete 1");
-        commandBox.runCommand("find Task77");
+        // find after completing one task
+        commandBox.runCommand("list");
+        commandBox.runCommand("complete 2");
+        assertFindExactMatchResult("find Task33");
+        assertFindExactMatchResult("find Task55", td.task55);
+    }
+
+    @Test
+    public void find_nearMatchCases() {
+        commandBox.runCommand("list");
+
+        // Levenshtein distance
+        assertFindExactMatchResult("find sk55");  // no results, missing two or more characters
+        assertFindExactMatchResult("find Task5566");  // no results, additional two or more characters
+        assertFindExactMatchResult("find ask55", td.task55); // missing one character
+        assertFindExactMatchResult("find Task555", td.task55);  // additional one character
+
+        // transposition error
+        assertFindExactMatchResult("find sTks55");  // no results, more than one pair of letters swapped
+        assertFindExactMatchResult("find Tsak55", td.task55); // only one pair of letters swapped
+
+        // contains
+        assertFindExactMatchResult("find 555");  // 555 is not within Task55
+        assertFindExactMatchResult("find 55", td.task55); // 55 is within Task55
     }
 
     @Test
     public void find_emptyList() {
         commandBox.runCommand("clear");
-        assertFindResult("find Jean"); // no results
+        assertFindExactMatchResult("find Task"); // no results
     }
 
     @Test
     public void find_invalidCommand_fail() {
-        commandBox.runCommand("findgeorge");
+        commandBox.runCommand("findtask");
         assertResultMessage(Messages.MESSAGE_UNKNOWN_COMMAND);
     }
 
-    private void assertFindResult(String command, TestTask... expectedHits) {
+    private void assertFindExactMatchResult(String command, TestTask... expectedHits) {
         commandBox.runCommand(command);
         assertListSize(expectedHits.length);
         assertResultMessage(expectedHits.length
                 + " Task(s) listed! Including " + expectedHits.length
                 + " Exact Match case(s) & 0 Near Match case(s).");
+        assertTrue(taskListPanel.isListMatching(expectedHits));
+    }
+
+    private void assertFindNearMatchResult(String command, TestTask... expectedHits) {
+        commandBox.runCommand(command);
+        assertListSize(expectedHits.length);
+        assertResultMessage(expectedHits.length
+                + " Task(s) listed! Including 0 Exact Match case(s) & "
+                + expectedHits.length + " Near Match case(s).");
         assertTrue(taskListPanel.isListMatching(expectedHits));
     }
 }
